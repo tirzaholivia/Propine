921776b4ab2310cf3a93d5562c2960351e441477
Olivia <tirzaholivia14@gmail.com>
2022-01-15 20:25:18 -0800

    First Commit

---
+++ Olivia/src/Test/year5Digits.java
@@ -0,0 +1,42 @@
+
+package Test;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class year5Digits  {
+
+	public WebDriver driver;
+	@Test
+	public void yr5dgts()
+	{
+	driver.findElement(By.xpath("//input[@class='form-control']")).sendKeys("12-mar-20223");
+	driver.findElement((By.xpath("//input[@type='submit']"))).click();
+	WebElement op = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div"));
+	String txt = op.getText();
+	System.out.println("Output: "+txt);
+    String value = "Invalid date";
+	Assert.assertTrue(txt.contains(value));
+	}
+
+	 @BeforeClass
+	    public void before5()
+	    {
+	    	System.setProperty("webdriver.chrome.driver", "/Users/vikki/Documents/chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get("https://vast-dawn-73245.herokuapp.com/");
+			
+	    }
+	   @AfterClass
+	   public void after5()
+	   {
+		   driver.quit();
+	   }
+}
